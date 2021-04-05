98acde6b712d516016585365eb55d908b7a4f1c7
Matthew Goad <matthewxgoad@gmail.com>
2021-04-04 19:37:59 -0500

    created dummy readme file to establish desired layout

---
+++ dummy-readme.md
@@ -0,0 +1,38 @@
+# Project Title
+
+## Table of Contents
+
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [Questions?](#Questions?)
+
+## Description
+
+Spicy jalapeno officia esse magna pork consequat porchetta capicola ullamco pariatur eiusmod. Swine ut drumstick tenderloin kielbasa consectetur dolor. Drumstick minim pork cow et qui nulla pork loin tri-tip ribeye esse salami turkey. Fatback corned beef consectetur pastrami ipsum venison labore.
+
+## Installation
+
+Spicy jalapeno officia esse magna pork consequat porchetta capicola ullamco pariatur eiusmod. Swine ut drumstick tenderloin kielbasa consectetur dolor. Drumstick minim pork cow et qui nulla pork loin tri-tip ribeye esse salami turkey. Fatback corned beef consectetur pastrami ipsum venison labore.
+
+## Usage
+
+Spicy jalapeno officia esse magna pork consequat porchetta capicola ullamco pariatur eiusmod. Swine ut drumstick tenderloin kielbasa consectetur dolor. Drumstick minim pork cow et qui nulla pork loin tri-tip ribeye esse salami turkey. Fatback corned beef consectetur pastrami ipsum venison labore.
+
+## License
+
+Spicy jalapeno officia esse magna pork consequat porchetta capicola ullamco pariatur eiusmod. Swine ut drumstick tenderloin kielbasa consectetur dolor. Drumstick minim pork cow et qui nulla pork loin tri-tip ribeye esse salami turkey. Fatback corned beef consectetur pastrami ipsum venison labore.
+
+## Contributing
+
+Spicy jalapeno officia esse magna pork consequat porchetta capicola ullamco pariatur eiusmod. Swine ut drumstick tenderloin kielbasa consectetur dolor. Drumstick minim pork cow et qui nulla pork loin tri-tip ribeye esse salami turkey. Fatback corned beef consectetur pastrami ipsum venison labore.
+
+## Tests
+
+Spicy jalapeno officia esse magna pork consequat porchetta capicola ullamco pariatur eiusmod. Swine ut drumstick tenderloin kielbasa consectetur dolor. Drumstick minim pork cow et qui nulla pork loin tri-tip ribeye esse salami turkey. Fatback corned beef consectetur pastrami ipsum venison labore.
+
+## Questions
+
+Spicy jalapeno officia esse magna pork consequat porchetta capicola ullamco pariatur eiusmod. Swine ut drumstick tenderloin kielbasa consectetur dolor. Drumstick minim pork cow et qui nulla pork loin tri-tip ribeye esse salami turkey. Fatback corned beef consectetur pastrami ipsum venison labore.
--- index.js
+++ index.js
@@ -2,54 +2,12 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 ​
 const prompts = [
-    // Project Title
-    {
-        type: 'input',
-        message: 'What is your project title?',
-        name: 'projectTitle'
-    },
-    // Project Description
-    {
-        type: 'input',
-        message: 'What is a short description of your project?',
-        name: 'description'
-    },
-    // Installation Instructions
-    {
-        type: 'input',
-        message: 'What are the installation instructions?',
-        name: 'installInstructions'
-    },
-    // Usage Information
-    {
-        type: 'input',
-        message: 'What is the usage information?',
-        name: 'usageInfo',
-    },
-    // Constribution Guidelines
-    {
-        type: 'input',
-        message: 'What are the contribution guidelines?',
-        name: 'contribGuide',
-    },
-    // Test Instructions
-    {
-        type: 'input',
-        message: 'What are the test instructions?',
-        name: 'testInstructions',
-    },
-    // License Selection
-    {
-        type: 'list',
-        message: 'Please choose a license:',
-        choices: 
-        name: 'license',
-    },
     // GitHub Username
     {
         type: 'input',
         message: 'What is your GitHub username?',
         name: 'githubName',
+        // Validate to ensure user has entered something for this prompt.
         validate: function (answer) {
             if (answer.length < 1) {
                 return console.log("You must enter a GitHub username.");
@@ -61,6 +19,72 @@ const prompts = [
         type: 'input',
         message: 'What is your email address?',
         name: 'email',
+        // Validate to ensure user has entered something for this prompt.
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a email address.");
+            }
+            return true;
+    },
+    // Project Title
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'projectTitle'
+        // Validate to ensure user has entered something for this prompt.
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a project title.");
+            }
+            return true;
+    },
+    // Project Description
+    {
+        type: 'input',
+        message: 'What is a short description of your project?',
+        name: 'description'
+        // Validate to ensure user has entered something for this prompt.
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a description.");
+            }
+            return true;
+    },
+    // Installation Instructions
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installInstructions'
+        // Optional prompt; not validation required.
+    },
+    // Usage Information
+    {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usageInfo',
+        // Optional prompt; not validation required.
+    },
+    // Constribution Guidelines
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribGuide',
+        // Optional prompt; not validation required.
+    },
+    // Test Instructions
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'testInstructions',
+        // Optional prompt; not validation required.
+    },
+    // License Selection
+    {
+        type: 'list',
+        message: 'Please choose a license. If unsure, go to choosealicense.com/',
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib'],
+        name: 'license',
+        // Optional prompt; not validation required.
     },
 ]
 ​
